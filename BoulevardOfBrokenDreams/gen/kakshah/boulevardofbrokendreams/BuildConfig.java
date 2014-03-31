9f931ac43314dd2fb6c3a35f15eb250739234208
kakshah <kakshah@gmail.com>
2014-03-31 22:18:30 +0530

    Initial commit

---
+++ BoulevardOfBrokenDreams/gen/kakshah/boulevardofbrokendreams/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kakshah.boulevardofbrokendreams;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Scroll/gen/kakshah/android/scroll/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kakshah.android.scroll;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
